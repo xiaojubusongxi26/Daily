66e3c985324bc8b1de23ce96eafd5e8b3fe522d4
XiaoJu <1600549245@qq.com>
2022-03-01 17:41:15 +0800

    热部署

--- daily/.vuepress/config.js
+++ daily/.vuepress/config.js
@@ -27,6 +27,16 @@ module.exports = {
         "type": 'text/css'
       }
     ],
+    ['meta', { name: 'keywords', content: '小橘不颂兮的个人博客，前端，计算机，摄影，学习，向前走吧' }],
+    ['meta', { name: 'author', content: '小橘不颂兮' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }  ],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/144.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/144.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     // [
     //   'link',
     //   { rel: 'stylesheet', href: './public/assets/css/fontawesome.css' }
@@ -117,4 +127,13 @@ module.exports = {
       lang: 'zh-CN'
     }
   },
+  plugins: [
+    '@vuepress/pwa',  {
+      serviceWorker: true,
+      updatePopup: { // 更改刷新内容的弹窗的显示文字
+        message: "内容有更新啦~",
+        buttonText: "我看看"
+      }
+    }
+  ]
 }
