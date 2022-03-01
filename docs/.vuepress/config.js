c7f3d33063dc29a1dcaec04db40f1453bd7d6bdd
XiaoJu <1600549245@qq.com>
2022-03-01 22:05:26 +0800

    修改路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,13 +29,13 @@ module.exports = {
     ],
     ['meta', { name: 'keywords', content: '小橘不颂兮的个人博客，前端，计算机，摄影，学习，向前走吧' }],
     ['meta', { name: 'author', content: '小橘不颂兮' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'manifest', href: './public/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }  ],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/144.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/144.png', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/144.png' }],
+    ['link', { rel: 'apple-touch-icon', href: './public/icons/144.png' }],
+    ['link', { rel: 'mask-icon', href: './public/icons/144.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: './public/icons/144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     // [
     //   'link',
