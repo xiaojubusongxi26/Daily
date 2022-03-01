9e3e0fe248ce81aa830b78c4a4924f441a620717
XiaoJu <1600549245@qq.com>
2022-03-01 19:51:59 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:xiaojubusongxi26/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xiaojubusongxi26/Daily.git master:main
+git push -f git@github.com:xiaojubusongxi26/Daily.git master:gh-pages
 
 cd -
