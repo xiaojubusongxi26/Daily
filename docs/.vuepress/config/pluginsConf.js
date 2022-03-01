ca21f935bef4ef0207bcc0650b2ddf7640f8e261
XiaoJu <1600549245@qq.com>
2022-03-01 22:34:38 +0800

    添加动态标题插件

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -6,5 +6,13 @@ module.exports = {
       message: "内容有更新啦~",
       buttonText: "我看看"
     }
+  },
+  "dynamic-title":
+  {
+     showIcon: "/favicon.ico",
+     showText: "(/≧▽≦/)咦！又好了！",
+     hideIcon: "/favicon.ico",
+     hideText: "(●—●)喔哟，崩溃啦！",
+     recoverTime: 2000
   }
 }
